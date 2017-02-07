71507be1c72f1fa0c96233d6214d7c87732f3d5e
chengzhelin <chengzhelin@stargame.com>
2017-02-07 17:31:44 +0800

    修改接口，调整样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,7 +20,7 @@ Page({
       duration: 10000
     })
     wx.request({
-      url: 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1',
+      url: 'http://gank.io/api/random/data/%E7%A6%8F%E5%88%A9/10',
       header: {
         'content-type': 'application/json'
       },
